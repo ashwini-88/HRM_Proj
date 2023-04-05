465120b511ffda97cacde8a0987a0773c9172b31
91917 <91917@Ashwini>
2023-04-05 15:29:38 +0530

    change made in test case_01

--- src/test/java/maven_Practice/TestCase_01.java
+++ src/test/java/maven_Practice/TestCase_01.java
@@ -1,16 +1,13 @@
 package maven_Practice;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestCase_01 {
 
 	@Test(groups = "smoke")
 	private void Ts_01() {
 		System.out.println("---Test Script_01 is Executed---");
-		WebDriver driver = WebDriverManager.chromedriver().create();
-        driver.get("https://www.facebook.com/");
+		//WebDriver driver = WebDriverManager.chromedriver().create();
+        //driver.get("https://www.facebook.com/");
 	}
 }
