5b414acdae2de0e8de806551e592d0d76393d924
91917 <91917@Ashwini>
2023-04-06 15:23:56 +0530

    Spaces removed

--- src/test/java/maven_Practice/TestCase_01.java
+++ src/test/java/maven_Practice/TestCase_01.java
@@ -6,7 +6,7 @@ public class TestCase_01 {
 
 	@Test(groups = "smoke")
 	private void Ts_01() {
-		System.out.println("---Test Script_01 is Executed      ---");
+		System.out.println("---Test Script_01 is Executed---");
 		//WebDriver driver = WebDriverManager.chromedriver().create();
         //driver.get("https://www.facebook.com/");
 	}
